323b3ff09d02585fb54a200b7ca77016e2e0cae3
Henri <32584105+OctopusPie@users.noreply.github.com>
2022-05-10 11:19:28 +0200

    Repaired LoadBalancer

--- webservice3/src/main/java/com/example/webservice3/LoadBalancer.java
+++ webservice3/src/main/java/com/example/webservice3/LoadBalancer.java
@@ -1,20 +1,19 @@
 package com.example.webservice3;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Webservice3Application {
+public class LoadBalancer {
     @Autowired
     private LoadBalancerClient loadBalancer;
 
     @GetMapping("/")
     public void method() {
 
-        ServiceInstance serviceInstance = loadBalancer.choose("microservice1");
+        ServiceInstance serviceInstance = loadBalancer.choose("webservice1");
 
         System.out.println(serviceInstance.getUri());
     }
--- webservice3/src/test/java/com/example/webservice3/LoadBalancerTests.java
+++ webservice3/src/test/java/com/example/webservice3/LoadBalancerTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Webservice3ApplicationTests {
+class LoadBalancerTests {
 
     @Test
     void contextLoads() {
